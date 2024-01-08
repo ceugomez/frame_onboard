908d310cb0c21f8c7a630a47b7b898ba3e59b833
cgf <cego6160@colorado.edu>
2024-01-07 17:53:40 -0700

    remove submodule entry

--- build/bno055/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/ceu/Desktop/nodes-onboard/install/bno055'
--- build/lightwarelidar2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ceu/Desktop/nodes-onboard/src/lightwarelidar2
-# Build directory: /home/ceu/Desktop/nodes-onboard/build/lightwarelidar2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
