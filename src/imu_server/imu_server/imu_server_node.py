6702810e78153777e1890dc2a4f84b1d182c22fb
cgf <cego6160@colorado.edu>
2024-01-10 12:35:51 -0700

    ^.

---
+++ src/imu_server/imu_server/imu_server_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from imu_server.')
+
+
+if __name__ == '__main__':
+    main()
--- src/imu_server/setup.py
+++ src/imu_server/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'imu_server_node = imu_server.imu_server_node:main'
         ],
     },
 )
